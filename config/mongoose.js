75a4a7c2c581a53e922d6475f14e814f28ccb792
nthull <36854462+nthull@users.noreply.github.com>
2018-11-02 13:42:19 -0400

    Required
    Red

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function () {
-    var db = mongoose.connect(config.db, { useNewUrlParser: true });
+    var db = mongoose.connect(config.db, { useCreateIndex: true, useNewUrlParser: true });
 
     require('../app/models/user.server.model');
 
