2cbba7fd7fc4e66577e7cb194e1a3ab6c8403e78
rjv5171 <randyjva@gmail.com>
2018-11-30 13:43:51 -0500

    Added some css and html

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,6 @@
+var MongoClient = require('mongodb').MongoClient;
+//var url = "mongodb://localhost:27017/";
+
 exports.render = function (req, res) {
     if (req.session.lastVisit) {
         console.log(req.session.lastVisit);
@@ -10,3 +13,15 @@ exports.render = function (req, res) {
         userFullName: req.user ? req.user.fullName : ''
     });
 };
+
+/*MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("psuDB");
+    var url = MongoClient.connect{ useNewUrlParser: true };
+    dbo.collection("psuCourses").findOne({}, function (err, result) {
+        if (err) throw err;
+        console.log(result.name);
+        db.close();
+    });
+});
+*/
--- config/express.js
+++ config/express.js
@@ -32,6 +32,7 @@ module.exports = function() {
 	app.set('views', './app/views');
     app.set('view engine', 'ejs');
 
+    app.use(express.static(__dirname + '/public'));
     app.use(flash());
     app.use(passport.initialize());
     app.use(passport.session());
